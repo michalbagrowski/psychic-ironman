9d1856d20547cd663a309a1cb5505a65133bf36a
Michal  Bagrowski <michal@bagrowski.com>
2014-04-23 13:25:45 +0200

    mochiweb -> cowboy

--- dev/run.erl
+++ dev/run.erl
@@ -1,7 +1,18 @@
 -module(run).
-
 -export([start/0]).
 
 start() ->
-	ok = application:start(mochiweb),
+	start_with_deps(backend),
 	ok.
+
+start_with_deps(App) ->
+  case application:start(App) of
+    {error,{not_started,Deps}} ->
+		error_logger:info_msg("Starting deps: ~p for application: ~p~n", [Deps, App]),
+
+        start_with_deps(Deps),
+		start_with_deps(App);
+    ok ->
+		error_logger:info_msg("Starting  application: ~p~n",  [App]),
+		ok
+  end.

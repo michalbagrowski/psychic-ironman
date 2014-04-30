b5fc9b71ce8deeb0ac9f6ba1691e202c60b230c9
Michal  Bagrowski <michal@bagrowski.com>
2014-04-30 10:40:16 +0200

    fixing comments and tabs

--- apps/backend/src/socket_handler.erl
+++ apps/backend/src/socket_handler.erl
@@ -33,57 +33,53 @@ monitor()->
 	io:format("Monitoring backend_socket_dispatch: ~p with ref: ~p~n", [whereis(backend_socket_dispatch), Ref]).
 
 handle(Req, State) ->
-	io:format("~p~p~n", [Req,State]),
-	{ok, Req2} = cowboy_req:reply(200, [
-		{<<"content-type">>, <<"text/plain">>}
-		], <<"Hello Worlad!">>, Req),
-	{ok, Req2, State}.
+	  io:format("~p~p~n", [Req, State]),
+	  {ok, Req2} = cowboy_req:reply(200, [
+		    {<<"content-type">>, <<"text/plain">>}
+		    ], <<"Hello Worlad!">>, Req),
+	  {ok, Req2, State}.
 
 terminate(_Reason, _Req, _State) ->
-	ok.
+	  ok.
 
 websocket_init(_TransportName, Req, _Opts) ->
-	io:format("get new websocket connection ~n"),
+	  io:format("get new websocket connection ~n"),
     monitor(),
-	backend_socket_dispatch:add(self()),
+	  backend_socket_dispatch:add(self()),
     {ok, Req, undefined_state}.
 
 websocket_handle({text, Msg}, Req, State) ->
-	backend_socket_dispatch:send({text, Msg}, self()),
-    % {reply, {text, << "That's what she said! ", Msg/binary >>}, Req, State};
+    backend_socket_dispatch:send({text, Msg}, self()),
     {ok, Req, State};
 
 websocket_handle({binary, Msg}, Req, State) ->
-    io:format("Received binnary Msg. Forwarding to all clients"),
 	  backend_socket_dispatch:send({binary, Msg}, self()),
-    % {reply, {binnary, << "That's what she said! ", Msg/binary >>}, Req, State};
     {ok, Req, State};
 
 
 websocket_handle(_Data, Req, State) ->
-	io:format("websocket_handle: ~p~n", [_Data]),
+    io:format("websocket_handle: ~p~n", [_Data]),
     {ok, Req, State}.
 
 websocket_info({'DOWN', _, process, _, _}, _Req, _State) ->
-	monitor(),
-	io:format("Readding pid: ~p~n",[self()]),
-	backend_socket_dispatch:add(self()),
-	{ok, _Req, _State};
+    monitor(),
+	  io:format("Readding pid: ~p~n",[self()]),
+	  backend_socket_dispatch:add(self()),
+	  {ok, _Req, _State};
 
 websocket_info({text, Msg}, Req, State) ->
-  io:format("text message: ~p~n", [Msg]),
-  {reply, {text, << Msg/binary >>}, Req, State};
+    io:format("text message: ~p~n", [Msg]),
+    {reply, {text, << Msg/binary >>}, Req, State};
 
 websocket_info({binary, Msg}, Req, State) ->
-  io:format("binary message: ~p~~n", [Msg]),
-  {reply, {binary, Msg}, Req, State};
+    io:format("binary message: ~p~n", [Msg]),
+    {reply, {binary, Msg}, Req, State};
 
 websocket_info(Msg, Req, State) ->
-	io:format("UNKNOWN MESSAGE: ~p~n", [Msg]),
-  {ok, Req, State}.
-%% 	{reply, {text, <<"resend: " ,Msg/binary >>}, Req, State}.
+	  io:format("UNKNOWN MESSAGE: ~p~n", [Msg]),
+    {ok, Req, State}.
 
 websocket_terminate(_Reason, _Req, _State) ->
-	backend_socket_dispatch:remove( self()),
-	io:format("websocket_terminate: ~p~n", [self()]),
+	  backend_socket_dispatch:remove( self()),
+	  io:format("websocket_terminate: ~p~n", [self()]),
     ok.
